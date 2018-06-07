480a36e32dee02ecb6ac25db6299d71f7efe2272
Yushi Redhead <yushi.t.redhead@gmail.com>
2018-06-07 10:52:40 +0100

    [atlas construction] adding missing files

--- data/atlasing/deformetrica.sh
+++ data/atlasing/deformetrica.sh
@@ -5,6 +5,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
 
 # Atlas construction :
-deformetrica atlas 3D model.xml data_set.xml optimization_parameters.xml --output-dir=. 2&>1 deformetrica.log
+deformetrica atlas 3D model.xml data_set.xml optimization_parameters.xml --output-dir=. > deformetrica.log
 
 cat deformetrica.log | grep Log-like | awk -F" " '{print $3, $7, $11}' | tr -d ] > convergence.txt
