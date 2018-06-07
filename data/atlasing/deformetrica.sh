06b22939b40e2197cbcba76385acd3836bb7f332
Nicolas Toussaint <nicolas.a.toussaint@kcl.ac.uk>
2018-06-07 10:11:40 +0100

    [atlas-construction] adding the atlas construction notebook

---
+++ data/atlasing/deformetrica.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Moving to the local directory
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $dir
+
+# Atlas construction :
+deformetrica atlas 3D model.xml data_set.xml optimization_parameters.xml --output-dir=. 2&>1 deformetrica.log
+
+cat deformetrica.log | grep Log-like | awk -F" " '{print $3, $7, $11}' | tr -d ] > convergence.txt
